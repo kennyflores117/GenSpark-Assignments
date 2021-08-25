5cf43008a4127a13c68cddf65e6e4a50bc8f67c7
kennyflores117 <84090162+kennyflores117@users.noreply.github.com>
2021-08-25 13:17:17 -0400

    print board method

--- Noughts_and_Crosses/src/com/company/Main.java
+++ Noughts_and_Crosses/src/com/company/Main.java
@@ -1,8 +1,37 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        char[][] board = new char[3][3];
+
+        for (int i = 0; i < 3; i++)
+            for (int j = 0; j < 3; j++)
+                board[i][j] = ' ';
+
+        //Scanner sc = new Scanner(System.in);
+
+
+
+        printBoard(board);
+    }
+
+    public static void printBoard (char[][] board) {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (j == 2)
+                    System.out.print(board[i][j]);
+                else
+                    System.out.print(board[i][j] + " | ");
+            }
+            if (i == 2)
+                System.out.println();
+            else {
+                System.out.println();
+                System.out.println("---------");
+            }
+        }
     }
 }
