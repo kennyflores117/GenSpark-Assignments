911dd95e843b9b0f21776b64a665bd51b547c563
kennyflores117 <84090162+kennyflores117@users.noreply.github.com>
2021-08-25 14:39:04 -0400

    board position method

--- Noughts_and_Crosses/src/com/company/Main.java
+++ Noughts_and_Crosses/src/com/company/Main.java
@@ -1,21 +1,47 @@
 package com.company;
 
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
 
     public static void main(String[] args) {
         char[][] board = new char[3][3];
+        ArrayList<Integer> all_moves = new ArrayList<>(Arrays.asList(0, 1, 2, 3, 4, 5, 6, 7, 8));
 
         for (int i = 0; i < 3; i++)
             for (int j = 0; j < 3; j++)
                 board[i][j] = ' ';
 
-        //Scanner sc = new Scanner(System.in);
+        Random r = new Random();
+        int comp_move;
+        int[] board_position = new int[2];
 
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Welcome to Tic-Tac-Toe!\nDo you want to be X or O?\n");
 
+        boolean win, play = true;
+        char computer, player = sc.next().toUpperCase().charAt(0);
+        if (player == 'X') {
+            computer = 'O';
+            System.out.println("\nThe computer will go first.");
+        }
+        else {
+            computer = 'X';
+            System.out.println("\nYou will go first.");
+        }
+        //decide whether player or computer goes first.
+        while (play) {
+            if (player == 'X') {
+                comp_move = all_moves.get(r.nextInt(all_moves.size()));
+                boardPosition(comp_move, board_position);
+                all_moves.remove((Integer) comp_move);
+                board[board_position[0]][board_position[1]] = 'O';
 
-        printBoard(board);
+                System.out.print("What is your next move? (1-9)");
+            }
+            play = false;
+            printBoard(board);
+        }
     }
 
     public static void printBoard (char[][] board) {
@@ -26,12 +52,28 @@ public class Main {
                 else
                     System.out.print(board[i][j] + " | ");
             }
-            if (i == 2)
-                System.out.println();
-            else {
-                System.out.println();
+            System.out.println();
+            if (i < 2)
                 System.out.println("---------");
-            }
         }
+    }
+
+    public static void boardPosition (int choice, int[] board_position) {
+        if (choice < 3)
+            board_position[1] = 0;
+        else if (choice < 6)
+            board_position[1] = 1;
+        else
+            board_position[1] = 2;
+
+        board_position[0] = choice % 3;
+    }
+
+    public static void checkValidMove () {
+
+    }
+
+    public static boolean checkWinner () {
+        return true;
     }
 }
